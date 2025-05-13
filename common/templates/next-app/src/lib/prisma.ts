10af178b78f5326685697204aff3c2ea81299b3d
Andrej Griniuk <andrej.griniuk@gmail.com>
2025-05-13 15:41:33 +0300

    next app auth

--- common/templates/next-app/src/lib/prisma.ts
+++ common/templates/next-app/src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@/generated/prisma'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs';
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
